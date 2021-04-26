4cce07556614baf7d00feb41dc5119aa465d4606
Enzo Torres <ozne20015@gmail.com>
2021-04-26 10:33:11 -0300

    Projeto iniciado com brincadeira de coffee

---
+++ Primeiro/Coffee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primeiro
+{
+    class Coffee
+    {
+        public bool Status { set; get; }
+
+        public bool Empty()
+        {
+            if (Status == false)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public void Refill()
+        {
+            Status = true; 
+        }
+        public void Drink()
+        {
+            Status = false;
+        }
+    }
+}
---
+++ Primeiro/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coffee coffee = new Coffee();
+            Console.WriteLine("Is the coffee empty?[y/n]");
+            char answer = char.Parse(Console.ReadLine());
+            if (answer == 'n')
+            {
+                coffee.Refill();
+            }
+            else
+            {
+                Console.WriteLine("In this case I am going to refill it!");
+                coffee.Refill();
+            }
+
+            Console.WriteLine("Do qou want to drink it?[y/n]");
+            answer = char.Parse(Console.ReadLine());
+            if ( answer == 'y')
+            {
+                coffee.Drink();
+                Console.WriteLine("It is empty now!");
+            }
+            else
+            {
+                Console.WriteLine("Why not?are you nuts???");
+                Console.WriteLine("Get out of here!!!!");
+            }
+        }
+    }
+}
+
